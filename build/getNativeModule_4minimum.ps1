d3d629a8aee5319f8ae58a3a91ceaa5ec87168be
nobuoJT <mario.oga0512@gmail.com>
2025-04-28 14:59:19 +0900

    *fix [getNativeModule] nativeModuleの引き込み処理を修正し、親のnode_modulesを一時的にリネームするロジックを追加。最小構成再現のための環境隔離。x

--- build/getNativeModule_4minimum.ps1
+++ build/getNativeModule_4minimum.ps1
@@ -1,18 +1,23 @@
 ﻿# 最小構成でexeを動かすために、includeされないnativeModuleを同包したい。
 # dist/で.exeを実行して、moduleエラーが出たらdist/node_modulesに引っ張ってくる。
-
+try{
 # 1. dist/node_modules作成
 if (-not (Test-Path "dist/node_modules")) {
     New-Item -ItemType Directory -Path "dist/node_modules" | Out-Null
 }
 
+if ( (Test-Path "node_modules")) {
+    Write-Host "最小構成隔離のため、親のnode_modulesを一時的にリネーム"
+    Rename-Item -Path "node_modules" -NewName "_node_modules" | Out-Null
+}
 Write-Host "native module 引き込み開始"
+
 
 # 2. 繰り返し作業
 while ($true) {
     Write-Host "動作確認開始(不動時、正常動作後pause使用の可能性)"
     # 3. microShot.exe実行してエラーキャプチャ
-    $output = & dist\microShot.exe 2>&1
+    $output = & .\dist\microShot.exe 2>&1
 
     # nullチェック
     if (-not $output) {
@@ -34,7 +39,7 @@ while ($true) {
 
         Write-Host "▶ モジュールが見つかりません: $missingModule"
         
-        $sourcePath = "node_modules\$missingModule"
+        $sourcePath = "_node_modules\$missingModule"
         $destPath = "dist\node_modules\$missingModule"
 
         # 5. コピー
@@ -43,7 +48,7 @@ while ($true) {
             Copy-Item -Path $sourcePath -Destination $destPath -Recurse -Force
         }
         else {
-            Write-Host "⚠ モジュール $missingModule は node_modules に存在しません！"
+            Write-Host "⚠ モジュール $missingModule は _node_modules に存在しません！"
             break
         }
     }
@@ -51,4 +56,13 @@ while ($true) {
         Write-Host "✅ エラーが検出されません。完了！"
         break
     }
+}
+}
+
+finally{
+    if ( (Test-Path "node_modules")) {
+        Write-Host "最小構成隔離のため、一時的にリネームした親のnode_modulesを元に戻す"
+        Rename-Item -Path "_node_modules" -NewName "node_modules" | Out-Null
+    }
+
 }
